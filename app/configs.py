19e36f1603d3fd9890731b6d9b5baa7c40313920
Joey Dreijer <joey.dreijer@ing.nl>
2019-04-05 17:43:23 +0200

    Removed deprecated mongodb config

--- app/configs.py
+++ app/configs.py
@@ -1,16 +1,8 @@
 # Setting variables
 from app import app
 import os
-import datetime
 
-# Todo - set MongoDB username and Password as variables for DB
-app.config['SECRET_KEY'] = os.environ['C2_SECRET']
-app.config['C2_PORT'] = os.environ['C2_PORT']
-app.config['CELERY_BACKEND'] = os.environ['CELERY_BACKEND']
-app.config['CELERY_BROKER'] = os.environ['CELERY_BROKER']
-app.config['MONGODB_SETTINGS'] = {
-    'db': os.environ['MONGO_DB'],
-    'host': os.environ['MONGO_IP'],
-    'port': int(os.environ['MONGO_PORT'])
-}
-
+app.config['SECRET_KEY'] = os.getenv('C2_SECRET', None)
+app.config['C2_PORT'] = int(os.getenv('C2_PORT', 9090))
+app.config['CELERY_BACKEND'] = os.getenv('CELERY_BACKEND', 'redis://localhost:6379')
+app.config['CELERY_BROKER'] = os.getenv('CELERY_BROKER', 'redis://localhost:6379')
