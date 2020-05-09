00fc6e1e61ad1df5aa067af3dd7a609fec07e79d
Joey Dreijer <jdreijer@d3vzer0.com>
2020-05-09 17:40:59 +0200

    Added specific queue for workers and disabled empire3

--- workers/__init__.py
+++ workers/__init__.py
@@ -5,8 +5,12 @@ import logging
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 app = Celery('c2', backend=config['CELERY_BACKEND'], broker=config['CELERY_BACKEND'])
+app.conf.task_routes = {
+    'c2.*': { 'queue': 'c2' },
+    'api.*': { 'queue': 'api' }    
+}
+
 app.autodiscover_tasks([
-    'workers.empire3',
     'workers.empire2',
     'workers.system'
 ])
